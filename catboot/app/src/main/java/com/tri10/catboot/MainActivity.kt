8dc89647ae10c975225167edffdaee5ab325be6c
goncalopalaio <goncalo.palaio@gmail.com>
2022-11-23 20:56:52 +0000

    base project

---
+++ catboot/app/src/main/java/com/tri10/catboot/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.tri10.catboot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tri10.catboot.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ catboot/app/src/main/java/com/tri10/catboot/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.tri10.catboot.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
